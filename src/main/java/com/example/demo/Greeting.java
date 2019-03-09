d3115353ddc0c36f27df9619fcae4a35163616b4
EnriGaci <henrygaci@gmail.com>
2019-03-09 14:08:48 +0200

    Working version of adding a greeting to mysql database with crudreposetory handler

--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,23 +13,26 @@ public class Greeting {
 
 	@Id
 	@Column(name="id")
-	@GeneratedValue
-    private final long id;
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
 
 	@Column(name="name")
-	private final String name;
+	private String name;
 
-    public Greeting(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
